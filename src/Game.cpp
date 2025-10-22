b9c56e8569d2670b0e55f482be8c70ef118ccbe4
svan058 <svan058@users.sourceforge.net>
2025-10-22 20:30:25 +1100

    Revert "Fix multiplayer pause notification for ESC menu and Mentat"
    This reverts commit 469ebb64e78f4dd8fbb43ba0e1a58be09a0cc808.

--- src/Game.cpp
+++ src/Game.cpp
@@ -1386,13 +1386,6 @@ void Game::initializeNetwork() {
 
 void Game::resumeGame()
 {
-    // Notify other players in multiplayer if we're resuming from pause
-    if(bPause && gameType == GameType::CustomMultiplayer && pNetworkManager != nullptr) {
-        const std::string message = _("Game resumed!");
-        pInterface->getChatManager().addInfoMessage(message);
-        pNetworkManager->sendChatMessage(message);
-    }
-    
     bMenu = false;
     bPause = false;
 }
@@ -1494,13 +1487,6 @@ void Game::onOptions()
         pInGameMenu = std::make_unique<InGameMenu>((gameType == GameType::CustomMultiplayer), color);
         bMenu = true;
         pauseGame();
-        
-        // Notify other players in multiplayer
-        if(gameType == GameType::CustomMultiplayer && pNetworkManager != nullptr) {
-            const std::string message = _("Game paused!");
-            pInterface->getChatManager().addInfoMessage(message);
-            pNetworkManager->sendChatMessage(message);
-        }
     }
 }
 
@@ -1510,13 +1496,6 @@ void Game::onMentat()
     pInGameMentat = std::make_unique<MentatHelp>(pLocalHouse->getHouseID(), techLevel, gameInitSettings.getMission());
     bMenu = true;
     pauseGame();
-    
-    // Notify other players in multiplayer
-    if(gameType == GameType::CustomMultiplayer && pNetworkManager != nullptr) {
-        const std::string message = _("Game paused!");
-        pInterface->getChatManager().addInfoMessage(message);
-        pNetworkManager->sendChatMessage(message);
-    }
 }
 
 
